f1915db12a644cb97c79b00111252354545556ed
Arthur Miranda <drxmirand@gmail.com>
2026-06-02 09:49:04 -0300

    feat(p2):aprovados com pais phds

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Provabossini-p2
+
+# Alunos
+
+Arthur Silva Miranda RA: 
+
+Marcelo Augusto Luvizutto RA: 2041382221027
+
+Carlos RA:
--- p2_banco.sql
+++ p2_banco.sql
@@ -1,3 +1,4 @@
+--1
 CREATE TABLE tb_estudante (
     STUDENTID SERIAL PRIMARY KEY,
     FATHER_EDU INT,
@@ -8,3 +9,17 @@ CREATE TABLE tb_estudante (
     GRADE INT
 );  
 
+--2
+
+CREATE OR REPLACE PROCEDURE sp_aprovados_pais_phds()
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    v_contador INT;
+BEGIN
+    SELECT COUNT(*) FROM tb_estudante WHERE GRADE => 1 AND FATHER_EDU = 6 AND MOTHER_EDU = 6 INTO v_contador;
+    RAISE NOTICE '%', v_contador;
+END;
+$$;
+ 
+CALL sp_aprovados_pais_phds();
